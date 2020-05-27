c84859a5e73e4c24f312e24cb9271f283f83b262
TJX2014 <xiaoxingstack@gmail.com>
2020-05-27 19:44:16 +0800

    need to test stream sql from mysql

---
+++ flink-examples/flink-examples-table/src/main/java/org/apache/flink/table/examples/java/MysqlStreamTableJDBC.java
@@ -0,0 +1,41 @@
+package org.apache.flink.table.examples.java;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+public class MysqlStreamTableJDBC {
+
+
+	/**
+	 * debug
+	 * */
+	public static void main(String[] args) throws Exception {
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+
+		tEnv.executeSql("CREATE TABLE currency (\n" +
+			"  currency_id BIGINT,\n" +
+			"  currency_name STRING,\n" +
+			"  rate DOUBLE,\n" +
+			"  currency_timestamp  TIMESTAMP,\n" +
+			"  country STRING,\n" +
+			"  precise_timestamp TIMESTAMP(6),\n" +
+			"  precise_time TIME(6),\n" +
+			"  gdp DECIMAL(10, 6)\n" +
+			") WITH (\n" +
+			"   'connector' = 'jdbc',\n" +
+			"   'url' = 'jdbc:mysql://localhost:3306/flink',\n" +
+			"   'username' = 'root',\n" +
+			"   'password' = '123456',\n" +
+			"   'table-name' = 'currency',\n" +
+			"   'driver' = 'com.mysql.jdbc.Driver',\n" +
+			"   'lookup.cache.max-rows' = '500',\n" +
+			"   'lookup.cache.ttl' = '10s',\n" +
+			"   'lookup.max-retries' = '3')");
+		Table t = tEnv.sqlQuery("select * from currency");
+		t.execute();
+//		env.execute();
+
+	}
+}
