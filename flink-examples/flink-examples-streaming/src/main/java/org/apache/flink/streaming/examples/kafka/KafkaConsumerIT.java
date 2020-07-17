2950ccdfe4d0bea562c031e1f1dfa84d76020f78
TJX2014 <xiaoxingstack@gmail.com>
2020-07-17 08:51:21 +0800

    kafka integrate and try hbase

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/kafka/KafkaConsumerIT.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/kafka/KafkaConsumerIT.java
@@ -4,18 +4,33 @@ import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.connector.hbase.sink.HBaseSinkFunction;
+import org.apache.flink.connector.hbase.sink.LegacyMutationConverter;
+import org.apache.flink.connector.hbase.util.HBaseTableSchema;
 import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
 import org.apache.flink.connector.jdbc.JdbcSink;
 import org.apache.flink.formats.csv.CsvRowDeserializationSchema;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.connectors.kafka.KafkaTableSource;
 import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.types.Row;
+import org.apache.hadoop.conf.Configuration;
 
 import java.util.Properties;
+
+/**
+ nohup bin/kafka-server-start.sh config/server.properties > /tmp/logs &
+
+ bin/kafka-topics.sh --create --bootstrap-server localhost:9091 --topic test1 --partitions 1 --replication-factor 1
+
+ bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic test1 --partitions 1 --replication-factor 1
+
+ bin/kafka-console-producer.sh --topic test1 --broker-list localhost:9092
+ bin/kafka-console-consumer.sh --topic test1 --bootstrap-server localhost:9092
+
+* */
 
 public class KafkaConsumerIT {
     public static void main(String[] args) throws Exception {
@@ -23,32 +38,41 @@ public class KafkaConsumerIT {
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
         final TypeInformation<Row> rowInfo = Types.ROW(Types.STRING, Types.STRING);
         CsvRowDeserializationSchema schema = new CsvRowDeserializationSchema.Builder(rowInfo).build();
-        KafkaTableSource source = new KafkaTableSource(TableSchema.builder().build(), "test", properties, schema);
-        DataStream ds = source.getDataStream(env).filter(new FilterFunction<Row>() {
+        KafkaTableSource source = new KafkaTableSource(TableSchema.builder().build(), "test1", properties, schema);
+
+        DataStream<Row> ds = source.getDataStream(env).filter(new FilterFunction<Row>() {
             @Override
             public boolean filter(Row value) throws Exception {
                 return true;
             }
-        }).map(new MapFunction<Row, Object>() {
+        }).map(new MapFunction<Row, Row>() {
             @Override
-            public Object map(Row value) throws Exception {
-                return null;
+            public Row map(Row value) throws Exception {
+                return value;
             }
         });
-        final String INSERT_TEMPLATE = "insert into %s (id, title, author, price, qty) values (?,?,?,?,?)";
-        final String INPUT_TABLE = "books";
         ds.addSink(JdbcSink.sink(
-                String.format(INSERT_TEMPLATE, INPUT_TABLE),
+                "insert into books (id, title) values (?,?)",
                 (ps, t) -> {
                     ps.setInt(1, 111);
                     ps.setString(2, "hello");
                 },
                 new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
-                        .withUrl("")
-                        .withDriverName("")
+                        .withUsername("xiaoxing")
+                        .withPassword("123456")
+                        .withUrl("mysql:jdbc://localhost:3306")
+                        .withDriverName("com.mysql.jdbc.Driver")
                         .build()));
+
+//        Configuration conf = new Configuration();
+//        HBaseTableSchema hTableSchema = new HBaseTableSchema();
+//        new LegacyMutationConverter(hTableSchema);
+//        HBaseSinkFunction hSink = new HBaseSinkFunction("h111", conf,
+//                new LegacyMutationConverter(hTableSchema), 1, 1, 1);
+//        ds.addSink(hSink);
         ds.print();
         env.execute();
     }
